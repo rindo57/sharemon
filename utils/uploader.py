e4b084b9ff7f7781793f684e72750f85254fd839
rindo57 <77073061+rindo57@users.noreply.github.com>
2025-03-29 17:59:57 +0530

    Update uploader.py

--- utils/uploader.py
+++ utils/uploader.py
@@ -100,7 +100,7 @@ def initialize_cipher(key, iv, adata, tagsize):
 
 class PrivateBin:
     def __init__(self, message):
-        self._server = "https://paste.i2pd.xyz/"
+        self._server = "https://0.0g.gg/"
         self._version = 2
         self._compression = 'zlib'
         self._data = ''
